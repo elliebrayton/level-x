d8bae414298164db0380fed285af1900ff5d2e82
Ellie Brayton <ebrayton@ufl.edu>
2020-08-13 19:54:14 -0500

    Resolved CSS issues

--- js/main.js
+++ js/main.js
@@ -6,13 +6,10 @@ $('body').toggleClass('overflow');
 });
 
 
-var height = $('header').height();
-
-
   $("#header").backstretch([
-  "/images/blue_angels_fat_albert.jpg",
-  "/images/military.jpg",
-  "/images/military-men-departing-service-uniform-40820-450x300.jpeg"
+  "images/blue_angels_fat_albert.jpg",
+  "images/Military.jpg",
+  "images/military-men-departing-service-uniform-40820-450x300.jpeg"
 ], {
   duration: 1200,
   fade: 800,
@@ -21,29 +18,4 @@ var height = $('header').height();
     background: "rgba(24, 49, 91)",
     opacity: 0.71
   }
-});
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
 });
